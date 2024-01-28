60e76a04a07af2de4990077df621f5cbc2261244
AakarshAgg <aakarshaggarwal7@gmail.com>
2024-01-28 16:21:28 +0530

    cleanup our about us page

---
+++ src/Constants/CelebrityData.js
@@ -0,0 +1,38 @@
+import apj from "../assets/Images/QuotesPersonalityImage/apj.png";
+import billGates from "../assets/Images/QuotesPersonalityImage/billGates.png";
+import einstein from "../assets/Images/QuotesPersonalityImage/einstein.png";
+import nelsonMandela from "../assets/Images/QuotesPersonalityImage/nelsonMandela.png";
+import steveJobs from "../assets/Images/QuotesPersonalityImage/steveJobs.png";
+
+export const celebrities=[
+    {
+      title:"Nelson Mandela",
+      description:"Education is the most powerful tool you can use to change the world. ",
+      image:nelsonMandela,
+      slideNumber:1
+    },
+    {
+      title:"APJ Abdul Kalam",
+      description:"Education is the most powerful tool you can use to change the world. ",
+      image:apj,
+      slideNumber:2
+    },
+    {
+      title:"Albert Einstein",
+      description:"Education is the most powerful tool you can use to change the world. ",
+      image:einstein,
+      slideNumber:3
+    },
+    {
+      title:"Steve Jobs",
+      description:"Education is the most powerful tool you can use to change the world. ",
+      image:steveJobs,
+      slideNumber:4
+    },
+    {
+      title:"Bill Gates",
+      description:"Education is the most powerful tool you can use to change the world. ",
+      image:billGates,
+      slideNumber:5
+    },
+  ]
